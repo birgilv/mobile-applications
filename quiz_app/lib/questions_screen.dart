2f155182fe7ba572d18ccdc4c0d3ba8d848c09fb
birgilv <birgittevik84@gmail.com>
2024-09-13 15:20:58 +0200

    Added paddings to answers display

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -19,21 +19,26 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity, //Use as much space as possible
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center, //Center the content of the screen
-        children: [
-           Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40), //Adds margins for all sides
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //Center the content of the screen
+          crossAxisAlignment: CrossAxisAlignment.stretch, //Fills the screens width
+          children: [
+             Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center, //Center the text
             ),
-          ),
-          const SizedBox(height: 30,),
-          ...currentQuestion.answers.map((answer) { 
-            //... pulled out and added as induviual elements to the children list
-            return AnswerButton(answerText: answer, onTap: () {});
-          })
-        ],
+            const SizedBox(height: 30,),
+            ...currentQuestion.answers.map((answer) { 
+              //... pulled out and added as induviual elements to the children list
+              return AnswerButton(answerText: answer, onTap: () {});
+            })
+          ],
+        ),
       ),
     );
   }
