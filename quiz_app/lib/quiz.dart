a4a080cb32aa8d0978b3edc4b7db446e75d6aa5a
birgilv <birgittevik84@gmail.com>
2024-09-13 16:33:45 +0200

    Return back to startscreen when finished

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
@@ -14,7 +15,7 @@ class Quiz extends StatefulWidget {
 
 // By using the _ (underscore), the class become private
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -25,6 +26,12 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length){
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start_screen';
+      });
+    }
   }
 
   @override
