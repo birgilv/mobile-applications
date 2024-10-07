4c68b6d06ef6c4695829f3f447ec058ef7bb767d
birgilv <birgittevik84@gmail.com>
2024-10-07 19:13:35 +0200

    Refactored decoration

--- meals_app/lib/widgets/category_grid_item.dart
+++ meals_app/lib/widgets/category_grid_item.dart
@@ -1,13 +1,14 @@
-import 'package:meals_app/models/category.dart';
 import 'package:flutter/material.dart';
 
-class CategoryGridItem extends StatelessWidget {
-const CategoryGridItem({
-  super.key, 
-  required this.category,
-});
+import 'package:meals_app/models/category.dart';
 
-final Category category;
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
 
   @override
   Widget build(BuildContext context) {
@@ -16,23 +17,22 @@ final Category category;
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
-        padding: const EdgeInsets.all(60),
+        padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.55),
-              category.color.withOpacity(0.9),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          )
-        ),
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
         child: Text(
-          category.title, 
+          category.title,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
-            color: Theme.of(context).colorScheme.onSurface,
-          ),
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
         ),
       ),
     );
