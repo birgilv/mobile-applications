352567557cdcf0c9231ed85524053a2711240ef7
birgilv <birgittevik84@gmail.com>
2024-10-09 13:30:56 +0200

    Replacing screen instead of pushing

--- meals_app/lib/screens/filters.dart
+++ meals_app/lib/screens/filters.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/tabs.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -19,6 +21,17 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your filters'),
       ),
+      //drawer: MainDrawer(
+        //ofSelectScreen: (identifier) {
+          //Navigator.of(context).pop();
+          //if (identifier == 'meals') {
+            //Navigator.of(context).pushReplacement(
+              //MaterialPageRoute(
+                //builder: (ctx) => const TabsScreen(),
+              //),
+            //);
+          //}
+        //}),
       body: Column(
         children: [
           SwitchListTile(
