34c11fd11baf11a294beb92840ec1f0eb007980b
birgilv <birgittevik84@gmail.com>
2024-10-10 12:48:23 +0200

    Cleaned up imports

--- meals_app/lib/screens/tabs.dart
+++ meals_app/lib/screens/tabs.dart
@@ -1,11 +1,9 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/filters.dart';
-import 'package:meals_app/screens/meal_details.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/screens/random_meal.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
